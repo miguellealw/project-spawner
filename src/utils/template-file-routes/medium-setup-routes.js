913fe3fa36efb2f02048c4e782a4e85004eb4ed1
Miguel <miguellealwebdev@gmail.com>
2019-01-31 20:35:27 -0600

    Moved folders and files to src directory in medium setup

--- src/utils/template-file-routes/medium-setup-routes.js
+++ src/utils/template-file-routes/medium-setup-routes.js
@@ -13,80 +13,80 @@ const scssFiles = !shouldMakeFolderStructure
   ? [
       /* ============= SCSS - No Folder Structure ============= */
       {
-        template: 'medium-setup/styles/main2.scss.ejs',
-        target: `./${nameOfProject}/styles/main.scss`
+        template: 'medium-setup/src/styles/main2.scss.ejs',
+        target: `./${nameOfProject}/src/styles/main.scss`
       }
     ]
   : [
       /* ============= SCSS - Folder Structure ============= */
       {
-        template: 'medium-setup/styles/main.scss.ejs',
-        target: `./${nameOfProject}/styles/main.scss`
+        template: 'medium-setup/src/styles/main.scss.ejs',
+        target: `./${nameOfProject}/src/styles/main.scss`
       },
       /* SCSS - Helpers */
       {
-        template: 'medium-setup/styles/0-helpers/_0-helpers.scss.ejs',
-        target: `./${nameOfProject}/styles/0-helpers/_0-helpers.scss`
+        template: 'medium-setup/src/styles/0-helpers/_0-helpers.scss.ejs',
+        target: `./${nameOfProject}/src/styles/0-helpers/_0-helpers.scss`
       },
       {
-        template: 'medium-setup/styles/0-helpers/_functions.scss.ejs',
-        target: `./${nameOfProject}/styles/0-helpers/_functions.scss`
+        template: 'medium-setup/src/styles/0-helpers/_functions.scss.ejs',
+        target: `./${nameOfProject}/src/styles/0-helpers/_functions.scss`
       },
       {
-        template: 'medium-setup/styles/0-helpers/_mixins.scss.ejs',
-        target: `./${nameOfProject}/styles/0-helpers/_mixins.scss`
+        template: 'medium-setup/src/styles/0-helpers/_mixins.scss.ejs',
+        target: `./${nameOfProject}/src/styles/0-helpers/_mixins.scss`
       },
       {
-        template: 'medium-setup/styles/0-helpers/_variables.scss.ejs',
-        target: `./${nameOfProject}/styles/0-helpers/_variables.scss`
+        template: 'medium-setup/src/styles/0-helpers/_variables.scss.ejs',
+        target: `./${nameOfProject}/src/styles/0-helpers/_variables.scss`
       },
       /* SCSS - Plugins */
       {
-        template: 'medium-setup/styles/1-plugins/_1-plugins.scss.ejs',
-        target: `./${nameOfProject}/styles/1-plugins/_1-plugins.scss`
+        template: 'medium-setup/src/styles/1-plugins/_1-plugins.scss.ejs',
+        target: `./${nameOfProject}/src/styles/1-plugins/_1-plugins.scss`
       },
       /* SCSS - Base */
       {
-        template: 'medium-setup/styles/2-base/_2-base.scss.ejs',
-        target: `./${nameOfProject}/styles/2-base/_2-base.scss`
+        template: 'medium-setup/src/styles/2-base/_2-base.scss.ejs',
+        target: `./${nameOfProject}/src/styles/2-base/_2-base.scss`
       },
       {
-        template: 'medium-setup/styles/2-base/_global.scss.ejs',
-        target: `./${nameOfProject}/styles/2-base/_global.scss`
+        template: 'medium-setup/src/styles/2-base/_global.scss.ejs',
+        target: `./${nameOfProject}/src/styles/2-base/_global.scss`
       },
       {
-        template: 'medium-setup/styles/2-base/_reset.scss.ejs',
-        target: `./${nameOfProject}/styles/2-base/_reset.scss`
+        template: 'medium-setup/src/styles/2-base/_reset.scss.ejs',
+        target: `./${nameOfProject}/src/styles/2-base/_reset.scss`
       },
       {
-        template: 'medium-setup/styles/2-base/_state.scss.ejs',
-        target: `./${nameOfProject}/styles/2-base/_state.scss`
+        template: 'medium-setup/src/styles/2-base/_state.scss.ejs',
+        target: `./${nameOfProject}/src/styles/2-base/_state.scss`
       },
       {
-        template: 'medium-setup/styles/2-base/_typography.scss.ejs',
-        target: `./${nameOfProject}/styles/2-base/_typography.scss`
+        template: 'medium-setup/src/styles/2-base/_typography.scss.ejs',
+        target: `./${nameOfProject}/src/styles/2-base/_typography.scss`
       },
       /* SCSS - Layout */
       {
-        template: 'medium-setup/styles/3-layout/_3-layout.scss.ejs',
-        target: `./${nameOfProject}/styles/3-layout/_3-layout.scss`
+        template: 'medium-setup/src/styles/3-layout/_3-layout.scss.ejs',
+        target: `./${nameOfProject}/src/styles/3-layout/_3-layout.scss`
       },
       /* SCSS - Modules */
       {
-        template: 'medium-setup/styles/4-modules/_4-modules.scss.ejs',
-        target: `./${nameOfProject}/styles/4-modules/_4-modules.scss`
+        template: 'medium-setup/src/styles/4-modules/_4-modules.scss.ejs',
+        target: `./${nameOfProject}/src/styles/4-modules/_4-modules.scss`
       },
       /* SCSS - Templates */
       {
-        template: 'medium-setup/styles/5-templates/_5-templates.scss.ejs',
-        target: `./${nameOfProject}/styles/5-templates/_5-templates.scss`
+        template: 'medium-setup/src/styles/5-templates/_5-templates.scss.ejs',
+        target: `./${nameOfProject}/src/styles/5-templates/_5-templates.scss`
       }
     ]
 
 const cssFiles = [
   {
     template: 'simple-setup/styles/styles.css.ejs',
-    target: `./${nameOfProject}/styles/styles.css`
+    target: `./${nameOfProject}/src/styles/styles.css`
   }
 ]
 
@@ -99,16 +99,16 @@ const cssFilename = cssPathArr[cssPathArr.length - 1];
 module.exports = [
   /* ============= HTML ============= */
   {
-    template: 'medium-setup/index.html.ejs',
-    target: `./${nameOfProject}/index.html`,
+    template: 'medium-setup/src/index.html.ejs',
+    target: `./${nameOfProject}/src/index.html`,
     props: {
       nameOfProject: startCase(nameOfProject),
       cssFilename
     }
   },
   {
-    template: 'medium-setup/pages/about.html.ejs',
-    target: `./${nameOfProject}/pages/about.html`,
+    template: 'medium-setup/src/pages/about.html.ejs',
+    target: `./${nameOfProject}/src/pages/about.html`,
     props: {
       nameOfProject: startCase(nameOfProject),
       cssFilename
@@ -118,8 +118,8 @@ module.exports = [
   ...css,
   /* ============= JavaScript ============= */
   {
-    template: 'medium-setup/scripts/index.js.ejs',
-    target: `./${nameOfProject}/scripts/index.js`
+    template: 'medium-setup/src/scripts/index.js.ejs',
+    target: `./${nameOfProject}/src/scripts/index.js`
   },
   /* ============= JSON ============= */
   {
